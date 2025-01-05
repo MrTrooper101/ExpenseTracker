323a57184e03e57157631facd04be260c852c731
jenish101 <jenish.shakya333@gmail.com>
2025-01-05 19:58:25 +0545

    update profile and password menu added, settings menu modification in UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Dashboard from './components/dashboard/Dashboard.tsx';
 import Layout from './components/layout/Layout.tsx';
 import Expenses from './components/expenses/Expenses.tsx';
 import Reports from './components/reports/Reports.tsx';
+import ChangePassword from './components/changePassword/ChangePassword.tsx';
+import UpdateProfile from './components/update-profile/UpdateProfile.tsx';
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="/expenses" element={<Expenses />} />
           <Route path="/reports" element={<Reports />} />
+          <Route path="/settings/change-password" element={<ChangePassword />} />
+          <Route path="/settings/update-profile" element={<UpdateProfile />} />
         </Route>
       </Routes>
     </Router>
